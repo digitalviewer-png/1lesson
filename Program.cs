622fec229403c25556cfacdfd14babae2c0f1448
Roman Pyatnov <romaandbears@gmail.com>
2023-05-26 23:27:53 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите сумму вклада: ");
+double a = double.Parse(Console.ReadLine());
+if (a <= 100)
+{
+    Console.WriteLine(a * 1.05);
+}
+else if (a > 100 && a < 200)
+{
+    Console.WriteLine(a * 1.07);
+}
+else 
+{
+    Console.WriteLine(a * 1.1);
+}
