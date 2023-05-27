02cd6beeb694d469053bf63caf0b83b0ad0d0c51
Roman Pyatnov <romaandbears@gmail.com>
2023-05-27 12:46:37 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Введите сумму вклада: ");
+﻿/*Console.Write("Введите сумму вклада: ");
 double a = double.Parse(Console.ReadLine());
 if (a <= 100)
 {
@@ -12,4 +12,91 @@ else
 {
     Console.WriteLine(a * 1.1);
 }
-// ff
+*/
+/*Console.Write("Введите количество долларов: ");
+double dollars = double.Parse(Console.ReadLine());
+double uan = 6.75;
+Console.WriteLine($"Столько будет долларов в юанях:{dollars + uan} ");
+*/
+
+
+// Домашняя Работа
+
+// Задача 1
+
+/*
+
+Console.Write("Введите темепературу по шкале Цельсия: ");
+double a = double.Parse(Console.ReadLine());
+double Farengate = 32, b = 5;
+double Kelvin = 273.13;
+Console.WriteLine($"Температура по фаренгейту:{(a - Farengate) * 5}");
+Console.WriteLine($"Температура по кельвину: {a + Kelvin}");
+
+*/
+// Задача 2
+
+/*
+ 
+Console.WriteLine("Привет, я калькулятор!"); Console.WriteLine("Я умею выполнять операции: \n 1.сложения \n 2.вычитания \n 3.умножения \n 4.деления \n 5.вычисления остатка от деления. \n");
+Console.Write("Напишите число от 1 до 5, в зависимости от требуемой операции: ");
+double a = double.Parse(Console.ReadLine());
+if (a == 1)
+{
+    Console.WriteLine("Введите два числа: ");
+    double b = double.Parse(Console.ReadLine());
+    double c = double.Parse(Console.ReadLine());
+    Console.WriteLine(b + c);
+}
+else if (a == 2)
+{
+    Console.WriteLine("Введите два числа: ");
+    double b = double.Parse(Console.ReadLine());
+    double c = double.Parse(Console.ReadLine());
+    Console.WriteLine(b - c);
+}
+else if (a == 3)
+{
+    Console.WriteLine("Введите два числа: ");
+    double b = double.Parse(Console.ReadLine());
+    double c = double.Parse(Console.ReadLine());
+    Console.WriteLine(b * c);
+}
+else if (a == 4)
+{
+    Console.WriteLine("Введите два числа: ");
+    double b = double.Parse(Console.ReadLine());
+    double c = double.Parse(Console.ReadLine());
+    Console.WriteLine(b / c);
+}
+else if (a == 5)
+{
+    
+    Console.WriteLine("Введите два числа: ");
+    double b = double.Parse(Console.ReadLine());
+    double c = double.Parse(Console.ReadLine());
+    Console.WriteLine(b % c);
+    
+}
+else
+{
+    Console.WriteLine("Введите число от 1 до 5!");
+}*/
+
+//Задача 3
+/*Console.WriteLine("Привет, я могу проверить твои знания таблицы умножения! \n Напиши мне два целых однозначных числа: ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = a * b;
+Console.WriteLine("Напиши мне свой ответ: ");
+int answer = int.Parse(Console.ReadLine());
+if (answer == c)
+{
+    Console.WriteLine($"Всё верно! Твой ответ: {c}");
+}
+else
+{
+    Console.WriteLine($"К сожалению ты ошибся, правильный ответ {c} \n Не расстраивайся и подучи таблицу умножения ;)");
+}*/
+
+// Задача 4 (1-4 год, 5-20 лет, 21 год, 22-24 года,25 - 30 лет 31 год, 32 - 34 года...)
